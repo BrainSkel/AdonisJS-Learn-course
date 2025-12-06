8f174c28603a445270b85abae88107681ef95544
BrainSkel <hanne53rik@gmail.com>
2025-12-06 16:34:55 +0200

    learned about routes and bit abt views

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,16 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+
+router.on("/news").render("pages/news");
+
+
+
+// router.get('/news',({ view }) => {
+//      return view.render('pages/news')
+// });
+
+// router.get("/", async ({ view }) => {
+//   return "hello world";
+// });
