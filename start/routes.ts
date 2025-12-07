feb4eeed183671c08f29d8477b176509a03ce5dd
BrainSkel <hanne53rik@gmail.com>
2025-12-07 20:32:44 +0200

    added exceptions for Insomnia testing

--- start/routes.ts
+++ start/routes.ts
@@ -14,8 +14,18 @@ router.on('/').render('pages/home')
 
 router.on("/news").render("pages/news");
 
+router.post("/news", ( {request, response} ) => {
+    const { email, password } = request.body();
+    console.log(email, password);
+    //return {email, password};
+    return response.redirect().toPath('/news');
+});
 
 
+ router.patch("/news/:id", ( {params} ) => {
+    console.log(params);
+     return "patch rotue";
+ });
 // router.get('/news',({ view }) => {
 //      return view.render('pages/news')
 // });
